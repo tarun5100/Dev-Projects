b44708909d019bbf3d9e3f18f4abc81dc7aee3df
Tarun Sood <tsood05@gmail.com>
2020-11-04 14:29:17 +0530

    Project code Deployed

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/space-x-project'));
+app.use(express.static(__dirname + '/dist/spaceX-project'));
 app.get('/*',function(req,res){
-    res.sendFile(path.join(__dirname+ '/dist/space-x-project/index.html'));});
+    res.sendFile(path.join(__dirname+ '/dist/spaceX-project/index.html'));});
     app.listen(process.env.PORT || 8080);
